f4dd462fb2dab9c0e522c9dfa0b4935461fe7277
Yogesh Pal <yogeshpal5049@gmail.com>
2025-06-17 15:53:27 +0530

    variable changes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,16 +3,15 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlT5zVRrVjuAb59X-CWdwOXTcdJ5JyOzc",
-  authDomain: "expense-tracker-bdce7.firebaseapp.com",
-  projectId: "expense-tracker-bdce7",
-  storageBucket: "expense-tracker-bdce7.firebasestorage.app",
-  messagingSenderId: "658330829878",
-  appId: "1:658330829878:web:39368e6cfa12445692c497",
-  measurementId: "G-ZJ8GSKCEYK",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
